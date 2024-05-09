92b12f6805c91e044fb32a79dab8f6609707ddc4
nate-yorio <nathan.yorio@vinted.com>
2024-05-09 16:17:35 +0300

    actually add the terraform file

---
+++ lua/terraform.lua
@@ -0,0 +1,42 @@
+-- Stole this from https://www.mukeshsharma.dev/2022/02/08/neovim-workflow-for-terraform.html
+
+vim.cmd [[silent! autocmd! filetypedetect BufRead,BufNewFile *.tf]]
+vim.cmd [[autocmd BufRead,BufNewFile *.hcl set filetype=terraform]]
+vim.cmd [[autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl]]
+vim.cmd [[autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform]]
+vim.cmd [[autocmd BufRead,BufNewFile *.tfstate,*.tfstate.backup set filetype=json]]
+
+-- Automatically terraform fmt and align on save
+vim.cmd [[let g:terraform_fmt_on_save=1]]
+vim.cmd [[let g:terraform_align=1]]
+
+-- Keymaps for running terragrunt from inside nvim
+vim.keymap.set('n', '<leader>ti', ':!terragrunt init<CR>')
+vim.keymap.set('n', '<leader>tv', ':!terragrunt validate<CR>')
+vim.keymap.set('n', '<leader>tp', ':!terragrunt plan<CR>')
+vim.keymap.set('n', '<leader>taa', ':!terragrunt apply -auto-approve<CR>')
+
+-- Require terraform language and tflint in lspconfig
+local lspconfig = require 'lspconfig'
+lspconfig.terraformls.setup {
+  cmd = { '/home/linuxbrew/.linuxbrew/bin/terraform-ls', 'serve' },
+  filetypes = { 'terraform', 'hcl' },
+  root_dir = lspconfig.util.root_pattern('.terraform', '.git'),
+}
+lspconfig.tflint.setup {
+  cmd = { '/home/linuxbrew/.linuxbrew/bin/tflint', '--langserver' },
+  filetypes = { 'terraform', 'hcl' },
+  root_dir = lspconfig.util.root_pattern('.terraform', '.git'),
+}
+-- not entirely sure this actually does anything
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+  pattern = { '*.tf', '*.tfvars', '*.hcl' },
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})
+
+-- Run this to get treesitter HCL grammar if it is not yet installed for some reason
+-- :TSInstall hcl
+-- brew install hashicorp/tap/terraform-ls
+-- brew install tflint
