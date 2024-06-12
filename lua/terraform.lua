ee5a367badf264c1aa8e0ca416ab1a5128c1d73a
nate-yorio <nathan.yorio@vinted.com>
2024-06-12 15:35:47 +0300

    terraform and spell

--- lua/terraform.lua
+++ lua/terraform.lua
@@ -1,6 +1,8 @@
--- Stole this from https://www.mukeshsharma.dev/2022/02/08/neovim-workflow-for-terraform.html
+-- Stolen and modified from https://www.mukeshsharma.dev/2022/02/08/neovim-workflow-for-terraform.html
 
 vim.cmd [[silent! autocmd! filetypedetect BufRead,BufNewFile *.tf]]
+-- Disabled because it was causing a bunch of messed up error highlighting for terragrunt
+-- due to the fact that terragrunt has blocks terraform does not support
 vim.cmd [[autocmd BufRead,BufNewFile *.hcl set filetype=terraform]]
 vim.cmd [[autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl]]
 vim.cmd [[autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform]]
@@ -15,7 +17,7 @@ vim.keymap.set('n', '<leader>tgi', ':!terragrunt init<CR>')
 vim.keymap.set('n', '<leader>tgv', ':!terragrunt validate<CR>')
 vim.keymap.set('n', '<leader>tgp', ':!terragrunt plan<CR>')
 vim.keymap.set('n', '<leader>tga', ':!terragrunt apply -auto-approve<CR>')
-vim.keymap.set('n', '<leader>tgf', ':!terragrunt hclfmt')
+vim.keymap.set('n', '<leader>tgf', ':cd %:p:h | !terragrunt hclfmt<CR>')
 
 -- Require terraform language and tflint in lspconfig
 local lspconfig = require 'lspconfig'
