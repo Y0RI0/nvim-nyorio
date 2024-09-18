335cfff7f00f5d086fbb946437a0bc197d5d16db
nate-yorio <nathan.yorio@vinted.com>
2024-09-18 14:35:42 +0300

    ObsidianTOC keymap

--- lua/keymaps/obsidian-keymap.lua
+++ lua/keymaps/obsidian-keymap.lua
@@ -1,3 +1,4 @@
 -- Obsidian Keymaps
 vim.keymap.set('n', '<leader>on', '<cmd>ObsidianNew<CR>', { desc = '[O]bsidian [n]ew' })
 vim.keymap.set('n', '<leader>ob', '<cmd>ObsidianBacklinks<CR>', { desc = '[O]bsidian [b]acklinks' })
+vim.keymap.set('n', '<leader>ot', '<cmd>ObsidianTOC<CR>', { desc = '[O]bsidian [T]able of contents' })
