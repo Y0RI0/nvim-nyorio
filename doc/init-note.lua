f6f8414748d10ec2ab6b2482fc4b2c99c3207138
nate-yorio <nathan.yorio@vinted.com>
2025-02-13 11:25:05 +0200

    move init note into a doc

---
+++ doc/init-note.lua
@@ -0,0 +1,15 @@
+--[[
+NOTE : 
+    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
+    which is very useful when you're not exactly sure of what you're looking for.
+
+  I have left several `:help X` comments throughout the init.lua
+    These are hints about where to find more information about the relevant settings,
+    plugins or Neovim features used in Kickstart.
+
+I hope you enjoy your Neovim journey,
+- TJ
+--]]
+
+-- Set <space> as the leader key
+-- See `:help mapleader`
--- init.lua
+++ init.lua
@@ -1,23 +1,6 @@
---[[
-NOTE : 
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-I hope you enjoy your Neovim journey,
-- TJ
---]]
-
--- Set <space> as the leader key
--- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
 require 'options' -- nvim settings, vim opts
