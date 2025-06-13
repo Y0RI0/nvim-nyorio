b4d6d33cc6f22a157799cf833d1918245e2f3c67
Y0RI0 <yorionathan@this-computer-kills-fascists>
2025-06-13 09:13:07 +0300

    deprecated options for obsidian.lua disabled

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -220,10 +220,12 @@ return {
 
     -- Optional, set to true if you use the Obsidian Advanced URI plugin.
     -- https://github.com/Vinzent03/obsidian-advanced-uri
-    use_advanced_uri = false,
+    -- deprecated,
+    -- use_advanced_uri = false,
 
     -- Optional, set to true to force ':ObsidianOpen' to bring the app to the foreground.
-    open_app_foreground = false,
+    -- deprecated
+    -- open_app_foreground = false,
 
     picker = {
       -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
